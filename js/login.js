304b83133dd742194ec5e17dd5717ce4e341b223
ckdgus1010 <ckdgus3735@gmail.com>
2025-07-26 23:49:51 +0900

    feat: 로컬 스토리지를 사용한 로그인 기능 구현
    - index.html에 로그인용 element 추가
    - 입력받은 사용자 이름을 local storage에 저장, 로드하는 기능 구현
    - style.css 추가

---
+++ js/login.js
@@ -0,0 +1,30 @@
+function login(event) {
+    event.preventDefault();
+
+    const username = loginForm.username.value;
+
+    localStorage.setItem(KEY_USERNAME, username);
+    showUsername(username);
+}
+
+function showUsername(username) {
+    const greeting = document.querySelector("#greeting");
+    
+    greeting.innerText = `Hello! ${username}`;
+    greeting.classList.remove(CLASS_HIDDEN);
+
+    loginForm.classList.add(CLASS_HIDDEN);
+}
+
+const CLASS_HIDDEN = "hidden";
+const KEY_USERNAME = "username";
+
+const loginForm = document.querySelector("#login-form");
+
+const username = localStorage.getItem(KEY_USERNAME);
+
+if (username) {
+    showUsername(username);
+} else {
+    loginForm.addEventListener("submit", login);
+}
