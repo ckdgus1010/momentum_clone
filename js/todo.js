d475097533788599371c9826728ee05e214f6868
ckdgus1010 <ckdgus3735@gmail.com>
2025-07-27 13:56:12 +0900

    feat: 로컬 스토리지를 사용한 todo list 기능 구현
    - todo 등록, 삭제 기능 구현
    - todo 등록 전 trim()을 사용해 좌우 공백 제거
    - todo가 공백인 경우 등록이 안되도록 설정

---
+++ js/todo.js
@@ -0,0 +1,64 @@
+function enrollTodo(event) {
+    event.preventDefault();
+
+    const content = todoFormEl.todo.value.trim();
+    todoFormEl.todo.value = "";
+    
+    const todo = {
+        id: Date.now(),
+        content: content,
+    };
+
+    if (content !== "") {
+        saveTodo(todo);
+        draw(todo);
+    }
+}
+
+function saveTodo(todo) {
+    todos.push(todo);
+    localStorage.setItem(KEY_TODOS, JSON.stringify(todos));
+}
+
+function draw(todo) {
+    const span = document.createElement("span");
+    span.innerText = todo.content;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "❌";
+    deleteBtn.addEventListener("click", deleteTodo);
+
+    const li = document.createElement("li");
+    li.id = todo.id;
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    todoListEl.appendChild(li);
+}
+
+function deleteTodo(event) {
+    const parentEl = event.target.parentElement;
+    parentEl.remove();
+
+    todos = todos.filter((todo) => todo.id !== parseInt(parentEl.id));
+    localStorage.setItem(KEY_TODOS, JSON.stringify(todos));
+}
+
+const KEY_TODOS = "todos";
+
+let todos = [];
+
+const todoListEl = document.querySelector("#todo-list");
+const todoFormEl = document.querySelector("#todo-form");
+
+todoFormEl.addEventListener("submit", enrollTodo);
+
+const savedTodos = JSON.parse(localStorage.getItem(KEY_TODOS));
+
+if (savedTodos) {
+    savedTodos.forEach(todo => {
+        todos.push(todo);
+        draw(todo);
+    });
+}
